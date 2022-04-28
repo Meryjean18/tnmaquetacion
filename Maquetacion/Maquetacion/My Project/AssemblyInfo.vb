ee089ae76a79fe9ea0bc2bacf9dd8ec88de229e8
meryjean18 <merybettavilapoma@gmail.com>
2022-04-27 23:22:09 -0500

    Maquetación del home

---
+++ Maquetacion/Maquetacion/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general sobre un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie los valores de estos atributos para modificar la información
+' asociada a un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+<Assembly: AssemblyTitle("Maquetacion")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Maquetacion")>
+<Assembly: AssemblyCopyright("Copyright ©  2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID es para el Id. typelib cuando este proyecto esté expuesto a COM
+<Assembly: Guid("cd5634e5-b960-473d-b5b1-78d7af3a26f7")>
+
+' La información de versión de un ensamblado consta de los siguientes cuatro valores:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' Puede especificar todos los valores o aceptar los valores predeterminados de los números de compilación y de revisión 
+' mediante el carácter '*', como se muestra a continuación:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ Maquetacion/Maquetacion/assets/js/contacto.js
@@ -0,0 +1,16 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    iniciarApp();
+})
+
+function iniciarApp() {
+    configBotonEnviar();
+    
+}
+
+function configBotonEnviar() {
+    const boton = document.querySelector('.boton');
+    boton.addEventListener('click', function (e) {
+        e.preventDefault();
+        alert('Mensaje del boton');
+    })
+}
